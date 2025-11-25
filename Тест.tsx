a2ca7408418be72cab40fa9bf7a787cbb0f68f19
a-yunusov <a.yunusov5915@gmail.com>
2025-11-25 12:27:40 +0500

    Обновлена карта

---
+++ Тест.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Тест" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="tilesets/WA_Special_Zones.png" width="192" height="64"/>
+</tileset>
